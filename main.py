070f551d210a15fa62d52dda3441d3f18acb4d46
Rybeusz100 <43045686+Rybeusz100@users.noreply.github.com>
2026-02-01 22:57:10 +0100

    send notifications through ntfy.sh

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ import requests
 
 COMPS_FILE = 'comps.json'
 
-def get_upcoming_comps():
+def get_upcoming_comps(country: str):
     UPCOMING_COMPS_URL = 'https://www.worldcubeassociation.org/api/v0/competition_index'
-
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    country = config['country']
-    today = datetime.now()
 
     query_params = {
         'include_cancelled': 'false',
-        'ongoing_and_future': today.strftime('%Y-%m-%d'),
+        'ongoing_and_future': datetime.now().strftime('%Y-%m-%d'),
         'country_iso2': country
     }
 
@@ -35,25 +29,48 @@ def get_known_comps():
     except:
         return []
     
-def find_new_comps(known_comps, upcoming_comps):
+def find_new_comps(known_comps: list[dict], upcoming_comps: list[dict]):
     known_ids = [comp['id'] for comp in known_comps]
     upcoming_ids = [comp['id'] for comp in upcoming_comps]
     new_ids = [id for id in upcoming_ids if id not in known_ids]
     return [comp for comp in upcoming_comps if comp['id'] in new_ids]
 
-def write_comps(comps):
+def write_comps(comps: list[dict]):
     with open(COMPS_FILE, 'w') as f:
         json.dump([
             {
                 'id': comp['id'],
-                'name': comp['name']
+                'name': comp['name'],
+                'registration_open': comp['registration_open'],
+                'start_date': comp['start_date'],
+                'end_date': comp['end_date'],
+                'notification_sent': comp.get('notification_sent', False)
             } for comp in comps], f, indent=2)
+
+def send_notifications(comps: list[dict], topic: str):
+    for comp in comps:
+        if comp.get('notification_sent'):
+            continue
+        response = requests.post(f'https://ntfy.sh/{topic}',
+                      data=comp['name'],
+                      headers={
+                          'Title': f'Competition announced',
+                          'Click': f'https://www.worldcubeassociation.org/competitions/{comp['id']}'
+                      })
+        if response.ok:
+            comp['notification_sent'] = True
+            print(f'Sent notification - {comp['name']}')
     
 def main():
-    upcoming_comps = get_upcoming_comps()
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    upcoming_comps = get_upcoming_comps(config['country'])
     known_comps = get_known_comps()
     new_comps = find_new_comps(known_comps, upcoming_comps)
     print(f"Found {len(new_comps)} new comps")
-    write_comps(known_comps + new_comps)
+    all_comps = known_comps + new_comps
+    send_notifications(all_comps, config['ntfy_topic'])
+    write_comps(all_comps)
 
 main()
